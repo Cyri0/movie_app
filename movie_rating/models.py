9aaf6a15b79a00049e6c537357c4d89e868c298b
Nemes Tamás <nemes.tamas@mechwart.local>
2024-01-04 16:53:36 +0100

    some movies uploaded

--- movie_rating/models.py
+++ movie_rating/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    release = models.DateField()
+    poster_image = models.ImageField(upload_to='static/images/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    average_rating = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
